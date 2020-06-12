7d31b00d9624eeb2c67dcdfce881aeb3adf267ae
iamdeeban <dbin25@gmail.com>
2020-06-12 12:47:31 +0200

    math functions

---
+++ math.lua
@@ -0,0 +1,19 @@
+--basic operations with math lib
+
+answer = (2+5)*4
+print (answer) --28
+
+answer = 5%4 --modulo
+print (answer) --1
+
+myPi = math.pi
+print(myPi)
+
+myNumber = math.random() --normal random
+myNumber1 = math.random( 1,10) --random between range
+print (myNumber)
+print (myNumber1)
+
+-- trigonometric functions
+mySin = math.sin(1)
+print (mySin)
