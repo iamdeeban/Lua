4b60f8e689a1b9c4ff42d478b1e0fc34958816a7
iamdeeban <dbin25@gmail.com>
2020-06-12 11:58:26 +0200

    variables,comments,print

---
+++ print.lua
@@ -0,0 +1,18 @@
+--Using print and comments
+
+-- Single comment
+--[[ Block comments
+spanned over lines
+]]
+
+--simple print to console
+print("Hello world")
+
+--print numbers, no within quotes
+print(12)
+
+--print variable ; outputs nil
+print(hallo)
+
+--concat in print
+print("Hello" .. " " .. "World")
