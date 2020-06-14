2c30787af01b27d92e06d6770ce36e49a622a585
iamdeeban <dbin25@gmail.com>
2020-06-14 11:43:56 +0200

    Modules

---
+++ 15.Modules.lua
@@ -0,0 +1,6 @@
+--Modules implementation
+
+local myMod = require ("myModules")
+
+print(myMod.hi("A"))
+print(myMod.add(1,5))
---
+++ myModules.lua
@@ -0,0 +1,14 @@
+--contains table functions
+
+sample = {}
+
+function sample.add(n1,n2)
+    return n1 + n2
+end
+
+
+function sample.hi(name)
+    return "hi" ..name
+end
+
+return sample
